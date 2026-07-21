a5f199e9455df8200c89185eecd61618f591b6cf
ooliveiratg <menezesth16@gmail.com>
2026-07-21 17:45:25 -0300

    feat(security): add security settings

--- src/main/java/br/com/studyHub/security/SecurityConfig.java
+++ src/main/java/br/com/studyHub/security/SecurityConfig.java
@@ -2,10 +2,26 @@ package br.com.studyHub.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    SecurityF
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                .csrf(c -> c.disable())
+                .authorizeHttpRequests((authorize) -> {
+                            authorize
+                                    .requestMatchers("auth/login")
+                                    .permitAll()
+                                    .requestMatchers("auth/register")
+                                    .permitAll();
+                            authorize.anyRequest().authenticated();
+                        }
+
+                );
+        return http.build();
+    }
 }
